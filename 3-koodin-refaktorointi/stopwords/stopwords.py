c04bee04d726e5a9f94f650b0b483554c2ff41ba
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-11-06 14:52:12 +0200

    Use set operations for handling stopword sets

--- 3-koodin-refaktorointi/stopwords/stopwords.py
+++ 3-koodin-refaktorointi/stopwords/stopwords.py
@@ -1,20 +1,16 @@
 from nltk.corpus import stopwords
 
-fin_stops = set(stopwords.words('finnish'))
 fin_extras = set('''hei moi moikka moro tervehdys terve terveisin siis myös kiitos
          kiitoksia kiitosta ok eli okei no sitten jo vielä aina jotta'''.split())
-fin_stops.update(fin_extras)
 del_from_finstops = set('en et ei emme ette eivät'.split())
-for word in del_from_finstops: fin_stops.remove(word)
+fin_stops = set(stopwords.words('finnish')) - del_from_finstops | fin_extras
 
-swe_stops = set(stopwords.words('swedish'))
-swe_stops.remove('min')
+swe_stops = set(stopwords.words('swedish')) - set(('min',))
 
-en_stops = set(stopwords.words('english'))
 del_from_enstops = set('on as a d m o s t me no y'.split())
-for word in del_from_enstops: en_stops.remove(word)
+en_stops = set(stopwords.words('english')) - del_from_enstops
 
 email_stops = set('''mailto subject from to vs message
          original date re terv sent from kello'''.split())
-final_stops = fin_stops|email_stops # Stopwords used for text preprocessing
+final_stops = fin_stops | email_stops # Stopwords used for text preprocessing
 

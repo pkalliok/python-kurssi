cc69fa807e4a71169626cdd1034e67ea5311fae4
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-11-06 14:42:02 +0200

    tests for email and final stops

--- 3-koodin-refaktorointi/stopwords/test_stopwords.py
+++ 3-koodin-refaktorointi/stopwords/test_stopwords.py
@@ -14,3 +14,14 @@ def test_engstops():
     assert 'me' not in en_stops
     assert 'the' in en_stops
 
+def test_email_stops():
+    assert 'kello' in email_stops
+    assert 'madam' not in email_stops
+
+def test_final_stops():
+    assert 'okei' in final_stops
+    assert 'emme' not in final_stops
+    assert 'vai' in final_stops
+    assert 'kello' in final_stops
+    assert 'madam' not in final_stops
+

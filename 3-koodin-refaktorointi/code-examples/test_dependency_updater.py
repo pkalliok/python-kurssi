393b2f7758cfa00ee7b227e72cf74081919442e7
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-11-09 15:35:35 +0200

    Rewrite test_changed_obj_update with data_fixture

--- 3-koodin-refaktorointi/code-examples/test_dependency_updater.py
+++ 3-koodin-refaktorointi/code-examples/test_dependency_updater.py
@@ -31,12 +31,12 @@ def test_testdata():
         assert len(dep_lines) == 4
         for line in dep_lines: assert 'foo.obj' in line
 
-#def test_changed_obj_update():
-    #create_testdata('test2')
-    #dependency_updater.updateProjectFile('nonpnpapp',
-        #abspath('test2'), abspath('test2/objfiles'), 'Debug')
-    #dep_lines = list(get_dep_lines('test2'))
-    #assert len(dep_lines) == 4
-    #assert 'quux.obj' in dep_lines[1]
-    #assert 'foo.obj' not in dep_lines[1]
-    #destroy_testdata('test2')
+def test_changed_obj_update():
+    with data_fixture('test2') as d:
+        dependency_updater.updateProjectFile('nonpnpapp',
+                abspath(d), abspath(join(d,'objfiles')), 'Debug')
+        dep_lines = list(get_dep_lines(d))
+        assert len(dep_lines) == 4
+        assert 'quux.obj' in dep_lines[1]
+        assert 'foo.obj' not in dep_lines[1]
+

f8271a1c244ac38ce787d98a3f953e417a30e2d0
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-11-09 15:33:32 +0200

    Use context management protocol for test data fixtures

--- 3-koodin-refaktorointi/code-examples/test_dependency_updater.py
+++ 3-koodin-refaktorointi/code-examples/test_dependency_updater.py
@@ -1,7 +1,8 @@
 
 import shutil
-from os import getcwd
+from os import getcwd, chdir
 from os.path import join, exists, abspath
+from contextlib import contextmanager
 import dependency_updater
 
 def destroy_testdata(target_path):
@@ -11,16 +12,24 @@ def create_testdata(target_path):
     destroy_testdata(target_path)
     shutil.copytree('dependency_updater_test_data', target_path)
 
+@contextmanager
+def data_fixture(target_path):
+    save_cwd = getcwd()
+    test_dir = abspath(target_path)
+    create_testdata(test_dir)
+    yield test_dir
+    destroy_testdata(test_dir)
+    chdir(save_cwd)
+
 def get_dep_lines(target_path):
     return (line for line in open(join(target_path, "nonpnpapp.vcxproj"))
             if '<AdditionalDependencies>' in line)
 
 def test_testdata():
-    create_testdata('test1')
-    dep_lines = list(get_dep_lines('test1'))
-    assert len(dep_lines) == 4
-    for line in dep_lines: assert 'foo.obj' in line
-    destroy_testdata('test1')
+    with data_fixture('test1') as d:
+        dep_lines = list(get_dep_lines('test1'))
+        assert len(dep_lines) == 4
+        for line in dep_lines: assert 'foo.obj' in line
 
 #def test_changed_obj_update():
     #create_testdata('test2')

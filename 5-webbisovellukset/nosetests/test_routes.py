737104490371bbb3562f8c5a78d56eb5db7dbe11
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-11-23 15:10:19 +0200

    Root redirect & test

--- 5-webbisovellukset/nosetests/test_routes.py
+++ 5-webbisovellukset/nosetests/test_routes.py
@@ -6,3 +6,11 @@ def test_hello():
     assert get("http://localhost:3000/hello").text \
             == "Dead kittens and suffering"
 
+def test_nonexistent():
+    assert get("http://localhost:3000/nonexistent").status_code == 404
+
+def test_root_redirect():
+    resp = get("http://localhost:3000/")
+    assert resp.history[0].status_code == 302
+    assert 'kittens' in resp.text
+
--- 5-webbisovellukset/webapp.py
+++ 5-webbisovellukset/webapp.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
+
+@app.route("/")
+def root(): return redirect("/hello", code=302)
 
 @app.route("/hello")
 def hello(): return "Dead kittens and suffering"
